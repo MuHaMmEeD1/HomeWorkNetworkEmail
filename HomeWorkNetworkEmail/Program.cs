bd9a306a6ce1a67ea9ec48b7b7dd73071ca47e47
MuHaMmEeD1 <139770024+MuHaMmEeD1@users.noreply.github.com>
2024-03-31 05:52:37 +0400

    Add project files.

---
+++ HomeWorkNetworkEmail/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Net;
+
+HttpListener listener = new HttpListener();
+listener.Prefixes.Add("http://localhost:12345/");
+
+listener.Start();
+
+while (true) {
+
+    HttpListenerContext context = listener.GetContext();
+
+    HttpListenerRequest request = context.Request;
+    HttpListenerResponse response = context.Response;
+    response.ContentType = "text/html";
+
+    StreamWriter writer = new StreamWriter(response.OutputStream);
+
+    writer.WriteLine(File.ReadAllText("Pages/Email.html"));
+
+    for (int i = 0; i < request.QueryString.Count; i++)
+    {
+        Console.WriteLine(request.QueryString[i]);
+    }
+
+    writer.Dispose();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
